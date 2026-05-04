0d18c77d69d7353d4eab14ca24f980525c4e4909
RezaAkhoondzade <rezaakhoondzade@gmail.com>
2026-05-04 12:29:01 +0330

    [change] the styles of all long lines based on FILL and WRAP method

--- model.py
+++ model.py
@@ -2,24 +2,25 @@ import tensorflow as tf
 from tensorflow.keras import layers, models, regularizers
 
 
-def residual_block(x, filters, stride=1, weight_decay=1e-4):
+def residual_block(x, filters, stride=1, weight_decay=0e-4):
     shortcut = x
 
     # First convolution layer
     x = layers.Conv2D(filters, kernel_size=3, strides=stride, padding='same', 
-                      use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(x)
+        use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
 
     # Second convolution layer
     x = layers.Conv2D(filters, kernel_size=3, strides=1, padding='same', 
-                      use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(x)
+        use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(x)
     x = layers.BatchNormalization()(x)
 
     # Adjust shortcut if dimensions change
     if stride != 1 or shortcut.shape[-1] != filters:
-        shortcut = layers.Conv2D(filters, kernel_size=1, strides=stride, padding='same', 
-                                 use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(shortcut)
+        shortcut = layers.Conv2D(filters, kernel_size=1, strides=stride,
+            padding='same', use_bias=False,
+            kernel_regularizer=regularizers.l2(weight_decay))(shortcut)
         shortcut = layers.BatchNormalization()(shortcut)
 
     # Add shortcut to the main path
@@ -27,12 +28,14 @@ def residual_block(x, filters, stride=1, weight_decay=1e-4):
     x = layers.Activation('relu')(x)
     return x
 
-def build_resnet(input_shape=(32, 32, 3), num_classes=10, stage_blocks=[3, 3, 3], stage_filters=[16, 32, 64], weight_decay=1e-4):
-    inputs = layers.Input(shape=input_shape)
+def build_resnet(input_shape=(32, 32, 3), num_classes=10, stage_blocks=[3, 3, 3],
+        stage_filters=[16, 32, 64], weight_decay=1e-4):
 
     # Initial Convolution
-    x = layers.Conv2D(stage_filters[0], kernel_size=3, strides=1, padding='same', 
-                      use_bias=False, kernel_regularizer=regularizers.l2(weight_decay))(inputs)
+    inputs = layers.Input(shape=input_shape)
+    x = layers.Conv2D(filters=stage_filters[0], kernel_size=3, strides=1,
+        padding='same', use_bias=False,
+        kernel_regularizer=regularizers.l2(weight_decay))(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
 
@@ -46,7 +49,7 @@ def build_resnet(input_shape=(32, 32, 3), num_classes=10, stage_blocks=[3, 3, 3]
     # Global Average Pooling and Dense output
     x = layers.GlobalAveragePooling2D()(x)
     outputs = layers.Dense(num_classes, activation='softmax', 
-                           kernel_regularizer=regularizers.l2(weight_decay))(x)
+        kernel_regularizer=regularizers.l2(weight_decay))(x)
 
     model = models.Model(inputs, outputs)
     return model
