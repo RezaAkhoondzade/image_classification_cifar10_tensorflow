84430fb93c57d98c5d0db38df8aabcaf4794c6d9
RezaAkhoondzade <rezaakhoondzade@gmail.com>
2026-05-08 18:20:24 +0330

    Add resume training feature Add saving model loss and accuracy history and evaluation result on json files add STEPS_PER_EPOCH in config to use for different datasets and be static Change callsbacks to save best and last model together, best for evaluation, last for resume

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+import json
 import tensorflow as tf
 from dataset import get_datasets
 from model import build_resnet
 
 # Hyperparameters
-BATCH_SIZE = 128
+CHECKPOINT_DIR = "checkpoints/resnet333_try6"
+BATCH_SIZE = 64
 EPOCHS = 200
+STEPS_PER_EPOCH = 700
 STAGE_BLOCKS = [3, 3, 3]
 STAGE_FILTERS = [16, 32, 64]
 INITIAL_LR = 0.1
@@ -16,15 +19,19 @@ WEIGHT_DECAY = 0.0001
 
 def main():
 
-    # Prepare Checkpoint Folder
-    if not os.path.exists("checkpoint/"):
-        os.mkdir("checkpoint/")
+    # Prepare checkpoint folder and resume variables
+    resume = False
+    initial_epoch = 0
+    if not os.path.exists(CHECKPOINT_DIR):
+        os.mkdir(CHECKPOINT_DIR)
+    else:
+        resume = True
 
-    # Get Datasets
+    # Get datasets
     print("Preparing datasets...")
     train_dataset, val_dataset, test_dataset = get_datasets(batch_size=BATCH_SIZE)
 
-    # Build Model
+    # Build model
     print("Building model...")
     model = build_resnet(
         input_shape=(32, 32, 3),
@@ -35,11 +42,8 @@ def main():
     )
     model.summary()
 
-    # Learning Rate Scheduler
-    # Calculate decay steps based on training data size (45,000 samples)
-    steps_per_epoch = 45000 // BATCH_SIZE
-    decay_steps = steps_per_epoch * EPOCHS
-
+    # Learning rate scheduler
+    decay_steps = STEPS_PER_EPOCH * EPOCHS
     lr_scheduler = tf.keras.optimizers.schedules.CosineDecay(
         initial_learning_rate=INITIAL_LR,
         decay_steps=decay_steps,
@@ -59,31 +63,60 @@ def main():
         metrics=['accuracy']
     )
 
+    if resume:
+        checkpoint_path = os.path.join(CHECKPOINT_DIR, "last_checkpoint.keras")
+        print("Loading existing model checkpoint...")
+        model = tf.keras.models.load_model(checkpoint_path)
+        initial_epoch = int(model.optimizer.iterations.numpy() // STEPS_PER_EPOCH)
+        print(f"Continue training from epoch {initial_epoch}")
+
     # Callbacks
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-        filepath='checkpoint/best_resnet_weights.weights.h5',
+    checkpoint_cb_best = tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(CHECKPOINT_DIR, 'best_model.keras'),
         save_best_only=True,
-        save_weights_only=True,
+        save_weights_only=False,
         monitor='val_accuracy',
         mode='max',
         verbose=1
     )
 
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(
+            CHECKPOINT_DIR,
+            'last_checkpoint.keras'
+        ),
+        save_weights_only=False,
+        save_freq='epoch'
+    )
+
     # Training
     print("Starting training...")
-    history = model.fit(
+    model.fit(
         train_dataset,
         epochs=EPOCHS,
+        initial_epoch=initial_epoch,
+        steps_per_epoch=STEPS_PER_EPOCH,
         validation_data=val_dataset,
-        callbacks=[checkpoint_cb]
+        callbacks=[checkpoint_cb, checkpoint_cb_best]
     )
+
+    history_path = os.path.join(CHECKPOINT_DIR, "history.json")
+    with open(history_path, "w") as f:
+        json.dump(model.history.history, f)
 
     # Evaluation
     print("Evaluating on test set...")
     # Load the best weights before testing
-    model.load_weights('checkpoint/best_resnet_weights.weights.h5')
-    test_loss, test_acc = model.evaluate(test_dataset)
+    model = tf.keras.models.load_model(os.path.join(CHECKPOINT_DIR, 'best_model.keras'))
+    _, test_acc = model.evaluate(test_dataset)
     print(f"Test Accuracy: {test_acc:.4f}")
+
+    # Save evaluation results on test set
+    evaluation_dict = {"test_accuracy": test_acc}
+    evaluation_path = os.path.join(CHECKPOINT_DIR, "evaluation.json")
+    with open(evaluation_path, "w") as f:
+        json.dump(evaluation_dict, f)
+
 
 if __name__ == '__main__':
     main()
